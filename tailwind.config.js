e29c96cd822bc56a9e9808c7d75103b477fa0939
Kwabs-97 <khobewayne@gmail.com>
2023-03-30 03:54:32 +0000

    first form on mobile approach

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,28 @@
  */
 
 module.exports = {
-  content: ["./src**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Ubuntu: ["Ubuntu"],
+      },
+      backgroundImage: {
+        "bg-sidebar-mobile": "url('../assets/images/bg-sidebar-mobile.svg')",
+        "big-curvy-desktop": "url('../images/bg-curvy-desktop.svg')",
+      },
+      colors: {
+        Marine: "#02295A",
+        Purplish: "#513DFF",
+        Pastel: "#ADBEFF",
+        Light: "#BFE2FD",
+        Strawberry: "#ED3548",
+        Magnolia: "hsl(217, 100%, 97%)",
+        Alabaster: "hsl(231, 100%, 99%)",
+        Coolgray: "hsl(231, 11%, 63%)",
+        Lightgray: "hsl(229, 24%, 87%)"
+      },
+    },
   },
   plugins: [],
 };
